6d0e54b865d2eea3b4aa7fb0d89da95390e3e106
Spelkington <spelkington@gmail.com>
2019-10-06 08:50:17 -0600

    transcription box

--- WebBeethoven/src/index.js
+++ WebBeethoven/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// React Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+if(module.hot){
+  module.hot.accept()
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
